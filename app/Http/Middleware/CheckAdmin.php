8a3c2600990b40274c284bd922c862fe806aa24e
hiba alkhateeb <hibboalk@gmail.com>
2025-11-16 07:11:30 -1000

    add field role in admins

--- app/Http/Middleware/CheckAdmin.php
+++ app/Http/Middleware/CheckAdmin.php
@@ -1,20 +1,49 @@
 <?php
+//
+//namespace App\Http\Middleware;
+//
+//use Closure;
+//use Illuminate\Http\Request;
+//
+//class CheckAdmin
+//{
+//    public function handle(Request $request, Closure $next)
+//    {
+//        $user = $request->user();
+//
+//        if (!$user || !$user->is_admin) {
+//            return response()->json(['message' => 'Unauthorized'], 403);
+//        }
+//
+//        return $next($request);
+//    }
+//}
+
 
 namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class CheckAdmin
 {
+
     public function handle(Request $request, Closure $next)
     {
-        $user = $request->user();
 
-        if (!$user || !$user->is_admin) {
-            return response()->json(['message' => 'Unauthorized'], 403);
+
+        if (!empty(Auth::user()) && Auth()->user()->role != 1) {
+            return response()->json([
+                'status' => false,
+            ], 401);
+        } else {
+            return $next($request);
         }
 
-        return $next($request);
+
     }
+
+
 }
